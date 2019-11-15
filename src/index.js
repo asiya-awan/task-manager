9662c85f3cb04a8c64876476a6fa926d8cade468
Asia Awan <asia.awan@neovation.com>
2019-11-15 16:15:41 -0600

    passwordHasing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db/mongoose')
 const userRouter = require('./routers/users')
 const taskRouter = require('./routers/tasks')
+const bcrypt = require('bcryptjs')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,3 +15,16 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port: ', port)
 })
+
+const myFunction = async() => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
